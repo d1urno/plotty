04136647c8602a6ae6043a5fa4d79d20df01e60a
Pablo Miceli <d1urno@gmx.com>
2024-02-29 13:12:07 +0200

    feat: declare groups and custom characters in store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,10 +1,11 @@
 import { defineStore } from 'pinia'
 import { type Ref, ref } from 'vue'
-import type { Story } from '@/types/local'
+import type { CustomCharacterGroup, Story } from '@/types/local'
 import { useLocalStorage } from '@vueuse/core'
 import type { GenericToast } from '@/components/GenericToast.vue'
 import defaultStories from '@/constants/defaultStories'
 import type { AppModal } from '@/components/AppModal.vue'
+import { PresetCharacterGroups } from '@/constants/rules'
 
 // eslint-disable-next-line import/prefer-default-export
 export const useStore = defineStore('store', () => {
@@ -17,10 +18,23 @@ export const useStore = defineStore('store', () => {
   const isFirstTimeSettings: Ref<boolean> = useLocalStorage<boolean>('firstTimeSettings', true)
   const apiKey: Ref<string | undefined> = useLocalStorage<string | undefined>('apiKey', undefined)
 
+  const selectedCharacterGroupId: Ref<PresetCharacterGroups | string> = useLocalStorage<
+    PresetCharacterGroups | string
+  >('selectedCharacterGroupId', PresetCharacterGroups.RICK_AND_MORTY)
+
   const stories: Ref<Story[]> = useLocalStorage<Story[]>('stories', defaultStories, {
     serializer: {
       read: (v: string) => (v ? JSON.parse(v) : null),
       write: (v: Story[]) => JSON.stringify(v)
+    }
+  })
+
+  const customCharacterGroups: Ref<CustomCharacterGroup[]> = useLocalStorage<
+    CustomCharacterGroup[]
+  >('customCharacterGroups', [], {
+    serializer: {
+      read: (v: string) => (v ? JSON.parse(v) : null),
+      write: (v: CustomCharacterGroup[]) => JSON.stringify(v)
     }
   })
 
@@ -32,6 +46,8 @@ export const useStore = defineStore('store', () => {
     isAiLoading,
     appModal,
     appToasts,
-    isFirstTimeSettings
+    isFirstTimeSettings,
+    selectedCharacterGroupId,
+    customCharacterGroups
   }
 })
